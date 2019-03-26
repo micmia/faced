7412227441c6e297f73301f808f4584f7be34f7d
Chengbo Zhang <micmia128@gmail.com>
2019-03-27 00:00:49 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,61 @@
+import * as faceapi from 'face-api.js';
+
+function resizeCanvasAndResults(dimensions, canvas, results) {
+  const { width, height } = dimensions instanceof HTMLVideoElement
+    ? faceapi.getMediaDimensions(dimensions)
+    : dimensions;
+  canvas.width = width;
+  canvas.height = height;
+
+  // resize detections (and landmarks) in case displayed image is smaller than
+  // original size
+  return faceapi.resizeResults(results, { width, height });
+}
+
+function drawLandmarks(dimensions, canvas, results, withBoxes = true) {
+  const resizedResults = resizeCanvasAndResults(dimensions, canvas, results);
+
+  if (withBoxes) {
+    faceapi.drawDetection(canvas, resizedResults.map(det => det.detection));
+  }
+
+  const faceLandmarks = resizedResults.map(det => det.landmarks);
+  const drawLandmarksOptions = {
+    lineWidth: 2,
+    drawLines: true,
+    color: 'green'
+  };
+
+  faceapi.drawLandmarks(canvas, faceLandmarks, drawLandmarksOptions);
+}
+
+async function onPlay() {
+  const videoEl = $('#inputVideo').get(0);
+
+  if (videoEl.paused || videoEl.ended) {
+    return setTimeout(() => onPlay());
+  }
+
+  const options = new faceapi.TinyFaceDetectorOptions({ inputSize: 512, scoreThreshold: 0.5 });
+  const result = await faceapi.detectSingleFace(videoEl, options).withFaceLandmarks(true);
+  console.log(result);
+
+  if (result) {
+    drawLandmarks(videoEl, $('#overlay').get(0), [result], false);
+  }
+
+  setTimeout(() => onPlay());
+}
+
+async function run() {
+  await faceapi.loadTinyFaceDetectorModel('dist/models');
+  await faceapi.loadFaceLandmarkTinyModel('dist/models');
+  const stream = await navigator.mediaDevices.getUserMedia({ video: {} });
+  const videoEl = $('#inputVideo').get(0);
+  videoEl.srcObject = stream;
+  videoEl.onplay = function () {
+    onPlay();
+  };
+}
+
+run();
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public/dist')
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  }
+};
